7b43a0cf1e2d95df3af2390515cbbc9b11d8360a
suhui <suhui.bai@proinnova.com.cn>
2024-12-30 18:02:48 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
       '/api/': {
         // target: 'http://127.0.0.1:8088',   // 测试环境
         target: 'http://zpdianqi.cn:8088', // 产线地址
-        changeOrigin: true,  // 是否跨域
+        changeOrigin: false,  // 是否跨域
         pathRewrite: {
           '^/api/': ''
         }
